22f772ccbcefb95577dc092aa1e52cf9fc0e2a16
AsmaaZakaria <asmaazakaria94@gmail.com>
2019-12-07 23:33:31 +0200

    rename

--- pics/src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization: 'Client-ID 6709ec7427b5c95f498afa7ef915d4294b9a5b2fa36ec79e6352ebb313b7f4e9'
-  },
-});
